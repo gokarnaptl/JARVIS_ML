8b306136788fe2ceab2e87c57dbe19450f4dac87
Gokarna Patil <88316922+gokarnaptl@users.noreply.github.com>
2022-03-14 23:33:17 +0530

    Resolved conflicts

--- jarvis.py
+++ jarvis.py
@@ -2,6 +2,10 @@ from http.client import responses
 import random
 import json
 import torch
+from Brain import NeuralNet
+from NeuralNetwork import bag_of_words ,tokenize
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 with open("intents.json", 'r') as json_data:
 	intents = json.load(json_data)
 
@@ -9,6 +13,14 @@ FILE = "TrainData.pth"
 data = torch.load(FILE)
 
 input_size = data["input_size"]
+hidden_size = data["output_size"]
+output_size = data["output_size"]
+all_words = data["all_words"]
+tags = data["tags"]
+model_state = data["model_state"]
+
+model = NeuralNet(input_size,hidden_size,output_size).to(device)
+model.load_state_dict(model_state)
 model.eval()
 
 #----------------
@@ -27,6 +39,14 @@ def Main():
 	X = bag_of_words(sentence,all_words)
 	X = X.reshape(1,X.shape[0])
 	X = torch.from_numpy(X).to(device)
+
+	output = model(X)
+
+	_ , predicted = torch.max(output,dim=1)
+
+	tag = tags[predicted.item()]
+
+	probs = torch.softmax(output,dim=1)
 
 	prob = probs[0][predicted.item()]
 
